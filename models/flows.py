b5e373352311214ce06804b1a956a461d78b1cd4
Nirbhay Modhe <nirbhaym@gatech.edu>
2019-01-21 01:44:35 -0500

    minor changes

--- models/flows.py
+++ models/flows.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from models.layers import MaskedConv2d, MaskedLinear
+from sylvester_flows.models.layers import MaskedConv2d, MaskedLinear
 
 
 class Planar(nn.Module):
